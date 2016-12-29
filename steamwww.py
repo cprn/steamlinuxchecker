ae46fa112d44631a80a1075125c14db5af68fe7c
Cyprian Guerra <cyprian.guerra@gmail.com>
2016-12-29 03:16:30 +0100

    store API, fixes #1

--- steamwww.py
+++ steamwww.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+import pprint
 import requests
 import sys
 import time
@@ -10,7 +11,7 @@ class Scraper:
         self.__cache = PickleCache("~/tmp/steamwww_cache.pkl", 86400 * 90)
         self.__calls = 0
 
-    def get_html(self, id):
+    def get_html(self, id): # TODO: retire
         self.__calls += 1
         self.__calls % 20 == 0 and time.sleep(2) # sometimes wait between calls
         r = requests.get('http://store.steampowered.com/app/%s' % id, cookies={
@@ -22,13 +23,19 @@ class Scraper:
         assert r.text.find('enter your birth') < 0, "Age check error! %d" % id
         return r.text
 
-    # TODO: use DOM to read app platforms and ignore bundles
+    def get_json(self, id):
+        r = requests.get('http://store.steampowered.com/api/appdetails/?appids=%s' % id)
+        j = r.json()[str(id)]
+        assert r.status_code == 200, "Can't open %s (%d): %s" % (id, r.status_code, r.json())
+        assert j['success']
+        return j['data']
+
     def runs_on_linux(self, id, verbose = False):
         try:
             linux = self.__cache.get(id)['linux']
             verbose and print('   ', end = '')
         except KeyError:
-            linux = self.get_html(id).find("platform_img linux") > 0
+            linux = self.get_json(id)['platforms']['linux']
             self.__cache.add(id, {"linux": linux})
             self.__cache.save()
             verbose and print(' + ', end = '')
