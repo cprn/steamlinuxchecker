de9d478399f984158bb133eddfa47d93e409ce0e
Cyprian Guerra <cyprian.guerra@gmail.com>
2016-12-29 06:43:11 +0100

    linux support assumptions
    Assume Linux support for product types different than
    'game' (video, series, etc.); software is 'game' as well.
    
    Assume no Linux support if there's no dedicated store
    page or... steamstore unavailable + HTTP 200 (?).

--- steamwww.py
+++ steamwww.py
@@ -22,20 +22,23 @@ class Scraper:
         assert r.text.find('enter your birth') < 0, "Age check error! %d" % id
         return r.text
 
-    def get_json(self, id):
+    def get_json(self, id): # TODO: can take multiple comma separated ids
         time.sleep(1) # sleep between each call
         r = requests.get('http://store.steampowered.com/api/appdetails/?appids=%s' % id)
         j = r.json()[str(id)]
         assert r.status_code == 200, "Can't open %s (%d): %s" % (id, r.status_code, r.json())
-        assert j['success']
-        return j['data']
+        return j
 
     def runs_on_linux(self, id, verbose = False):
         try:
             linux = self.__cache.get(id)['linux']
             verbose and print('   ', end = '')
         except KeyError:
-            linux = self.get_json(id)['platforms']['linux']
+            try:
+                j = self.get_json(id)
+                linux = j['data']['platforms']['linux'] if j['data']['type'] == 'game' else True
+            except KeyError:
+                linux = False
             self.__cache.add(id, {"linux": linux})
             self.__cache.save()
             verbose and print(' + ', end = '')
